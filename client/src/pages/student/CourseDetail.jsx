efd8a33f407364be3da3a1dd41e7316fd08cc03f
Shvam Singh <singhshivam583@yahoo.in>
2024-12-15 21:54:23 +0530

    first commit

---
+++ client/src/pages/student/CourseDetail.jsx
@@ -0,0 +1,111 @@
+import BuyCourseButton from "@/components/BuyCourseButton";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { useGetCourseDetailWithStatusQuery } from "@/features/api/purchaseApi";
+import { BadgeInfo, Lock, PlayCircle } from "lucide-react";
+import React from "react";
+import ReactPlayer from "react-player";
+import { useNavigate, useParams } from "react-router-dom";
+
+const CourseDetail = () => {
+  const params = useParams();
+  const courseId = params.courseId;
+  const navigate = useNavigate();
+  const { data, isLoading, isError } =
+    useGetCourseDetailWithStatusQuery(courseId);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h>Failed to load course details</h>;
+
+  const { course, purchased } = data;
+  console.log(purchased);
+
+  const handleContinueCourse = () => {
+    if(purchased){
+      navigate(`/course-progress/${courseId}`)
+    }
+  }
+
+  return (
+    <div className="space-y-5">
+      <div className="bg-[#2D2F31] text-white">
+        <div className="flex flex-col gap-2 px-4 py-8 mx-auto max-w-7xl md:px-8">
+          <h1 className="text-2xl font-bold md:text-3xl">
+            {course?.courseTitle}
+          </h1>
+          <p className="text-base md:text-lg">Course Sub-title</p>
+          <p>
+            Created By{" "}
+            <span className="text-[#C0C4FC] underline italic">
+              {course?.creator.name}
+            </span>
+          </p>
+          <div className="flex items-center gap-2 text-sm">
+            <BadgeInfo size={16} />
+            <p>Last updated {course?.createdAt.split("T")[0]}</p>
+          </div>
+          <p>Students enrolled: {course?.enrolledStudents.length}</p>
+        </div>
+      </div>
+      <div className="flex flex-col justify-between gap-10 px-4 mx-auto my-5 max-w-7xl md:px-8 lg:flex-row">
+        <div className="w-full space-y-5 lg:w-1/2">
+          <h1 className="text-xl font-bold md:text-2xl">Description</h1>
+          <p
+            className="text-sm"
+            dangerouslySetInnerHTML={{ __html: course.description }}
+          />
+          <Card>
+            <CardHeader>
+              <CardTitle>Course Content</CardTitle>
+              <CardDescription>4 lectures</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-3">
+              {course.lectures.map((lecture, idx) => (
+                <div key={idx} className="flex items-center gap-3 text-sm">
+                  <span>
+                    {true ? <PlayCircle size={14} /> : <Lock size={14} />}
+                  </span>
+                  <p>{lecture.lectureTitle}</p>
+                </div>
+              ))}
+            </CardContent>
+          </Card>
+        </div>
+        <div className="w-full lg:w-1/3">
+          <Card>
+            <CardContent className="flex flex-col p-4">
+              <div className="w-full mb-4 aspect-video">
+                <ReactPlayer
+                  width="100%"
+                  height={"100%"}
+                  url={course.lectures[0].videoUrl}
+                  controls={true}
+                />
+              </div>
+              <h1>Lecture title</h1>
+              <Separator className="my-2" />
+              <h1 className="text-lg font-semibold md:text-xl">Course Price</h1>
+            </CardContent>
+            <CardFooter className="flex justify-center p-4">
+              {purchased ? (
+                <Button onClick={handleContinueCourse} className="w-full">Continue Course</Button>
+              ) : (
+                <BuyCourseButton courseId={courseId} />
+              )}
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseDetail;
