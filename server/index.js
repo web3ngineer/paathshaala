f06f9f2e2de66c04914d70415913f2354b1665cb
Shvam Singh <singhshivam583@yahoo.in>
2024-12-16 14:16:58 +0530

    update healthcheck route

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"https://paathshaala-pi.vercel.app/",
+    origin:"https://paathshaala-pi.vercel.app",
     credentials:true
 }));
  
