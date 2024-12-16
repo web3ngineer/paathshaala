fb59495d085d32d7292bea0ca031f4cf9d82bb0a
Shvam Singh <singhshivam583@yahoo.in>
2024-12-16 15:03:46 +0530

    update in cookies

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,6 +9,7 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
+      secure:true,
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     }).json({
